8aceef09bd041524a013c26629416e61f9daf88e
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-07 14:46:38 +0600

    refactor(jest config): convert from js to ts

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFiles: [".jest.setup.ts"],
+};
+module.exports = config;
