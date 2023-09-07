73068b5123ea4877e0d79a3c74e1bbf9ce2461ce
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-07 14:48:25 +0600

    refactor(.jest.setup.ts): add .jest.setup.ts

---
+++ .jest.setup.ts
@@ -0,0 +1,9 @@
+import assert from "assert";
+import dotenv from "dotenv";
+import { REQUIRED_PROCESS_ENVS } from "./__tests/requiredProcessEnvs";
+
+dotenv.config({ path: "./.env.test.local" });
+if (process.env === undefined) throw new Error("process.env is undefined");
+Object.values(REQUIRED_PROCESS_ENVS).forEach((env) => {
+  assert.notEqual(process.env[env], undefined);
+});
