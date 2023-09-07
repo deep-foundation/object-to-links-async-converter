5ccc73bd360f8317735316366b5078c9c439a5aa
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-07 17:03:58 +0600

    fix: fix import paths

--- src/debug.ts
+++ src/debug.ts
@@ -1,5 +1,5 @@
 import innerDebug from "debug";
-import { PACKAGE_NAME } from "./package-name";
+import { PACKAGE_NAME } from "./package-name.js";
 
 export function debug(namespace: string) {
   return innerDebug(`${PACKAGE_NAME}:${namespace}`);
--- src/required-packages-in-minilinks.ts
+++ src/required-packages-in-minilinks.ts
@@ -1,4 +1,4 @@
-import { PACKAGE_NAME } from "./package-name";
+import { PACKAGE_NAME } from "./package-name.js";
 
 export const REQUIRED_PACKAGES_IN_MINILINKS = [
   "@deep-foundation/core",
