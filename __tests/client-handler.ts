6c4126924b782162a44ba1a23f6d65b7bf7b6f25
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-07 15:32:26 +0600

    test(client-handler): apply required packages to minilinks

--- __tests/client-handler.ts
+++ __tests/client-handler.ts
@@ -9,14 +9,16 @@ import {
   ObjectToLinksConverterDecorator,
 } from "../src/create-object-to-links-converter-decorator.js";
 import { debug } from "../src/debug.js";
+import { PACKAGE_NAME } from "../src/package-name.js";
 
 const graphQlPath = `${process.env.DEEPLINKS_HASURA_PATH!}/v1/graphql`;
 const ssl = !!+process.env.DEEPLINKS_HASURA_SSL!;
 const token = process.env.DEEPLINKS_HASURA_TOKEN!;
 
 let apolloClient: ApolloClient<InMemoryCache>;
-let deep: DeepClient;
 let decoratedDeep: ObjectToLinksConverterDecorator<DeepClient>;
+
+const REQUIRED_PACKAGES_IN_MINILINKS = ["@deep-foundation/core", PACKAGE_NAME];
 
 beforeAll(async () => {
   apolloClient = generateApolloClient({
@@ -25,20 +27,24 @@ beforeAll(async () => {
     token,
     ws: true,
   });
-  deep = new DeepClient({ apolloClient });
+  const deep = new DeepClient({ apolloClient });
   decoratedDeep = createObjectToLinksConverterDecorator(deep);
 
-  const { data: corePackageLinks } = await deep.select({
+  const { data: requiredPackageLinks } = await deep.select({
     up: {
       tree_id: {
         _id: ["@deep-foundation/core", "containTree"],
       },
-      parent_id: {
-        _id: ["@deep-foundation/core"],
+      parent: {
+        _or: REQUIRED_PACKAGES_IN_MINILINKS.map((packageName) => ({
+          id: {
+            _id: [packageName],
+          },
+        })),
       },
     },
   });
-  deep.minilinks.apply(corePackageLinks);
+  decoratedDeep.minilinks.apply(requiredPackageLinks);
 });
 
 describe("without root link id with obj that has one string property", async function withoutRootLinkIdWithObjThatHasOneStringProperty() {
@@ -48,11 +54,11 @@ describe("without root link id with obj that has one string property", async fun
   };
   log({ obj });
   const clientHandlerResult = await callClientHandler({
-    deep,
+    deep: decoratedDeep,
     linkId: decoratedDeep.objectToLinksConverterPackage.clientHandler.idLocal(),
     args: [
       {
-        deep: deep,
+        deep: decoratedDeep,
         obj: obj,
       },
     ],
