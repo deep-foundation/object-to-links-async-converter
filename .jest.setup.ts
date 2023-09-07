42fa4b4c666adb45a4844b5bc4c20daaec10cfd6
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-07 15:08:30 +0600

    fix(.jest.setup.ts): fix path

--- .jest.setup.ts
+++ .jest.setup.ts
@@ -1,6 +1,6 @@
 import assert from "assert";
 import dotenv from "dotenv";
-import { REQUIRED_PROCESS_ENVS } from "./__tests/requiredProcessEnvs";
+import { REQUIRED_PROCESS_ENVS } from "./__tests/required-process-envs";
 
 dotenv.config({ path: "./.env.test.local" });
 if (process.env === undefined) throw new Error("process.env is undefined");
