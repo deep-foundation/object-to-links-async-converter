71840df363d6ccdbe862dc9df098c0936c22fd61
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-07 14:50:43 +0600

    ci(automation/.*ts): add automation scripts

---
+++ automation/documentation/generate.ts
@@ -0,0 +1,11 @@
+import { generateDocumentation } from "@deep-foundation/npm-automation";
+
+main();
+
+async function main() {
+  await generateDocumentation({
+    generateCliAppsHelpInReadmeOptions: null,
+    generateUsageWaysOfNpmCliAppsInMarkdownFormatOptions: null,
+    generateTableOfContentsForMarkdownOptions: null,
+  });
+}
