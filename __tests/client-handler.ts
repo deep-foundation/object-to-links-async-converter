4624287f02deeeb052aaf09a3f1fff9c2921c901
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-07 14:51:54 +0600

    test(client-handler): update client handler tests draft

--- __tests/client-handler.ts
+++ __tests/client-handler.ts
@@ -3,29 +3,29 @@ import { ApolloClient, InMemoryCache } from "@apollo/client/index.js";
 import "@testing-library/jest-dom";
 import assert from "assert";
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client.js";
+import { callClientHandler } from "../src/call-client-handler.js";
+import {
+  createObjectToLinksConverterDecorator,
+  ObjectToLinksConverterDecorator,
+} from "../src/create-object-to-links-converter-decorator.js";
 
-const graphQlPath = `${process.env.DEEPLINKS_HASURA_PATH}/v1/graphql`;
-if (process.env.DEEPLINKS_HASURA_SSL == null) {
-  throw new Error("DEEPLINKS_HASURA_SSL is not defined");
-}
-const ssl = !!+process.env.DEEPLINKS_HASURA_SSL;
-if (process.env.DEEPLINKS_HASURA_SECRET == null) {
-  throw new Error("DEEPLINKS_HASURA_SECRET is not defined");
-}
-const secret = process.env.DEEPLINKS_HASURA_SECRET;
-const ws = true;
+const graphQlPath = `${process.env.DEEPLINKS_HASURA_PATH!}/v1/graphql`;
+const ssl = !!+process.env.DEEPLINKS_HASURA_SSL!;
+const token = process.env.DEEPLINKS_HASURA_TOKEN!;
 
 let apolloClient: ApolloClient<InMemoryCache>;
 let deep: DeepClient;
+let decoratedDeep: ObjectToLinksConverterDecorator<DeepClient>;
 
 beforeAll(async () => {
   apolloClient = generateApolloClient({
     path: graphQlPath,
     ssl,
-    secret,
-    ws,
+    token,
+    ws: true,
   });
   deep = new DeepClient({ apolloClient });
+  decoratedDeep = createObjectToLinksConverterDecorator(deep);
 
   const { data: corePackageLinks } = await deep.select({
     up: {
@@ -41,5 +41,9 @@ beforeAll(async () => {
 });
 
 describe("client-handler", () => {
+  // await callClientHandler({
+  //   deep,
+  //   linkId: decoratedDeep.objectToLinksConverterPackage.handl,
+  // });
   throw new Error("Not implemented");
 });
