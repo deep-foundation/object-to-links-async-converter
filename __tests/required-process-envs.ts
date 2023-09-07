312fb8070da51577eb5d8ca4ec329048079cf707
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-07 14:50:13 +0600

    chore(required_process_envs): add REQUIRED_PROCESS_ENVS

---
+++ __tests/required-process-envs.ts
@@ -0,0 +1,5 @@
+export const REQUIRED_PROCESS_ENVS = {
+  deeplinksHasuraPath: "DEEPLINKS_HASURA_PATH",
+  deeplinksHasuraSsl: "DEEPLINKS_HASURA_SSL",
+  deeplinksHasuraSecret: "DEEPLINKS_HASURA_TOKEN",
+};
