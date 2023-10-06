276a2ef3fec9155848ab63940d35dd08e286b243
FreePhoenix888 <freephoenix888@gmail.com>
2023-10-07 00:39:09 +0600

    Add update handler

---
+++ src/handlers/update-handler.ts
@@ -0,0 +1,104 @@
+import { DeepClient } from "@deep-foundation/deeplinks/imports/client.js";
+import { Link } from "@deep-foundation/deeplinks/imports/minilinks.js";
+import { DeepClientInstance } from "@deep-foundation/deeplinks/imports/client";
+
+async (options: {
+  deep: DeepClient;
+  data: {
+    newLink: Link<number>;
+  };
+}) => {
+  const {
+    deep,
+    data: { newLink: rootLink },
+  } = options;
+  const { default: ts } = await import("typescript");
+  const { default: util } = await import("util");
+  try {
+    const result = await main();
+    return {
+      result: util.inspect(result),
+    };
+  } catch (error) {
+    throw {
+      error: util.inspect(error),
+    };
+  }
+
+  async function main() {
+    let obj;
+    if (typeof rootLink.value?.value === "object") {
+      obj = rootLink.value?.value;
+    } else if (typeof rootLink.value?.value === "string") {
+      try {
+        obj = JSON.parse(rootLink.value?.value);
+      } catch (error) {
+        throw new Error(
+          `##${rootLink.id} must be valid JSON if it is a string`,
+        );
+      }
+    } else {
+      throw new Error(`##${rootLink.id} must have value`);
+    }
+    if (!obj) {
+      throw new Error(`##${rootLink.id} must have value`);
+    }
+
+    const clientHandlerResult = await callClientHandler({
+      deep,
+      linkId: await deep.id(deep.linkId!, "clientHandler"),
+      args: [
+        {
+          deep: deep,
+          obj: obj,
+          rootLinkId: rootLink.id,
+          resultLinkId: rootLink.to_id,
+          // TODO?
+          // customMethods: options.customMethods,
+        },
+      ],
+    });
+
+    return clientHandlerResult;
+  }
+
+  async function callClientHandler(
+    options: CallClientHandlerOptions,
+  ): Promise<any> {
+    const { linkId, deep, args } = options;
+    const code = await deep
+      .select({
+        in: {
+          id: linkId,
+        },
+      })
+      .then((result) => {
+        const link = result.data[0];
+        if (!link)
+          throw new Error(`Unable to find SyncTextFile for ##${linkId}`);
+        const code = link.value?.value;
+        if (!code) throw new Error(`##${link.id} must have value`);
+        return code;
+      });
+
+    const functionExpressionString = ts
+      .transpileModule(code, {
+        compilerOptions: {
+          module: ts.ModuleKind.ESNext,
+          sourceMap: true,
+          target: ts.ScriptTarget.ESNext,
+        },
+      })
+      .outputText.replace("export {}", "");
+    const fn: Function = eval(functionExpressionString);
+
+    const result = await fn(...args);
+    return result;
+  }
+
+  interface CallClientHandlerOptions {
+    deep: DeepClientInstance;
+    linkId: number;
+    args: Array<any>;
+  }
+};
